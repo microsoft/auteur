36c4d95e55d18dd4303e49619ffa8cc9782cfef0
Mathieu Duponchelle <mathieu@centricular.com>
2021-08-17 03:34:17 +0200

    scripts/crossfade: make use of new API
    + use local playback
    
    + use connect configuration

--- scripts/crossfade.py
+++ scripts/crossfade.py
@@ -1,6 +1,6 @@
 import argparse
 
-from node_schedule import create_mixer, create_rtmp_destination, start_node, connect, schedule_source, add_control_point, later, get_info
+from node_schedule import create_mixer, create_local_playback_destination, start_node, connect, schedule_source, add_control_point, later, get_info
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('crossfade')
@@ -14,16 +14,15 @@ if __name__ == '__main__':
     dest_uri = args.dest_uri
 
     create_mixer('channel-1', 1920, 1080, 44100)
-    create_rtmp_destination('centricular-output', 'rtmp://learntv-transcoder.eastus.azurecontainer.io/live/centricular-output')
+    create_local_playback_destination('centricular-output')
     start_node('centricular-output')
     connect('channel-1', 'centricular-output')
     start_node('channel-1')
 
-    source_slot = schedule_source(source_uri, 'source_slot', 'channel-1', later(0), later(10))
-    dest_slot = schedule_source(dest_uri, 'dest_slot', 'channel-1', later(5))
-
-    add_control_point('control-1', source_slot, 'video::zorder', later(0), 2, interpolate=False)
-    add_control_point('control-1', dest_slot, 'video::zorder', later(0), 1, interpolate=False)
+    source_slot = schedule_source(source_uri, 'source_slot', 'channel-1', later(0), later(10),
+            slot_config={'video::zorder': 2, 'video::alpha': 1.0, 'audio::volume': 1.0})
+    dest_slot = schedule_source(dest_uri, 'dest_slot', 'channel-1', later(5),
+            slot_config={'video::zorder': 1, 'audio::volume': 0.0})
 
     add_control_point('control-1', source_slot, 'video::alpha', later(5), 1.0, interpolate=False)
     add_control_point('control-2', source_slot, 'video::alpha', later(10), 0.0)
