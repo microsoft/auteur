4e91f5f8c13f260f3ef4dd2a47993e56c525ba06
Mathieu Duponchelle <mathieu@centricular.com>
2022-03-09 23:28:43 +0100

    scripts: update web_overlay example

--- scripts/web_overlay.py
+++ scripts/web_overlay.py
@@ -1,6 +1,6 @@
 import argparse
 
-from node_schedule import create_mixer, create_rtmp_destination, start_node, connect, schedule_source, add_control_point, later, get_info
+from node_schedule import create_mixer, create_local_playback_destination, start_node, connect, schedule_source, add_control_point, later, get_info
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('web-overlay')
@@ -16,9 +16,9 @@ if __name__ == '__main__':
         'height': 720,
         'sample-rate': 44100
     })
-    create_rtmp_destination('centricular-output', 'rtmp://learntv-transcoder.eastus.azurecontainer.io/live/centricular-output')
-    start_node('centricular-output')
+    create_local_playback_destination('centricular-output')
     connect('channel-1', 'centricular-output')
+    start_node('centricular-output')
     start_node('channel-1')
 
     source_slot = schedule_source(source_uri, 'source_slot', 'channel-1', later(0),
